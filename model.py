98073038b6beeabdea7632ad3c33c9818fc5da6f
Mohammad Rezaei <rezaeimohammad.edu@gmail.com>
2021-08-06 17:13:20 +0430

    conv net turned to mlp

--- model.py
+++ model.py
@@ -2,29 +2,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-if(torch.cuda.is_available()):
-    device = torch.device('cuda')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(32*32*3, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 128)
+        self.fc4 = nn.Linear(128, 64)
+        self.fc5 = nn.Linear(64, 10)
+        self.dropout = nn.Dropout(0.25)
+
+
 
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(F.relu(x))
+        x = self.fc2(F.relu(x))
+        x = self.dropout(x)
+        x = self.fc3(F.relu(x))
+        x = self.dropout(x)
+        x = self.fc4(F.relu(x))
+        x = self.fc5(x)
         return x
-
 
     def train(self, trainloader, optimizer, criterion, epochs):
         hist = []
@@ -32,9 +35,7 @@ class Net(nn.Module):
             running_loss = 0.0
             for i, data in enumerate(trainloader, 0):
                 # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data
-                if(device):
-                    inputs, labels = inputs.to(device), labels.to(device)
+                inputs, labels = data[0].to(device), data[1].to(device)
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
@@ -62,9 +63,7 @@ class Net(nn.Module):
         # since we're not training, we don't need to calculate the gradients for our outputs
         with torch.no_grad():
             for data in testloader:
-                images, labels = data
-                if(device):
-                    images, labels = images.to(device), labels.to(device)
+                images, labels = data[0].to(device), data[1].to(device)
                 # calculate outputs by running images through the network
                 outputs = self(images)
                 # the class with the highest energy is what we choose as prediction
